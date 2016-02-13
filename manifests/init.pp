fc96660aac5bd4c0c2e847839ac4a0a9f1743060
John Florian <jflorian@doubledog.org>
2016-02-12 20:04:42 -0500

    New - mock::site_defaults parameter
    This provides a means for customizing mock's site-defaults configuration
    file.

--- manifests/init.pp
+++ manifests/init.pp
@@ -14,6 +14,10 @@
 #   Package state is to be 'installed' (default), 'absent', or any other value
 #   appropiate to a Package resource type.
 #
+# [*site_defaults*]
+#   Source URI providce mock's site-defaults configuration.  The default is to
+#   use the one provided by the package.
+#
 # === Authors
 #
 #   John Florian <jflorian@doubledog.org>
@@ -25,10 +29,23 @@
 
 class mock (
         $ensure='installed',
+        $site_defaults=undef,
     ) inherits ::mock::params {
 
     package { $::mock::params::packages:
         ensure => $ensure,
+    }
+
+    file { '/etc/mock/site-defaults.cfg':
+        ensure    => $ensure,
+        owner     => 'root',
+        group     => 'mock',
+        mode      => '0644',
+        seluser   => 'system_u',
+        selrole   => 'object_r',
+        seltype   => 'etc_t',
+        source    => $site_defaults,
+        subscribe => Package[$::mock::params::packages],
     }
 
 }
